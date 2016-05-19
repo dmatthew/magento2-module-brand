58c6b4729c720be3c894721eb36f6d74fb8c3073
Matthew Davis <mattd@copiousinc.com>
2016-05-18 18:08:27 -0700

    WIP: Begin creating brand view page.

---
+++ Block/Brand/View.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Dmatthew\Brand\Block\Brand;
+
+class View extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * Core registry
+     *
+     * @var \Magento\Framework\Registry
+     */
+    protected $_coreRegistry = null;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        array $data = []
+    ) {
+        $this->_coreRegistry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+
+        $this->pageConfig->getTitle()->set('Brands!!!');
+        
+        return $this;
+    }
+
+    /**
+     * Retrieve current brand model object
+     *
+     * @return \Dmatthew\Brand\Model\Brand
+     */
+    public function getBrand()
+    {
+        if (!$this->hasData('current_brand')) {
+            $this->setData('current_brand', $this->_coreRegistry->registry('current_brand'));
+        }
+        return $this->getData('current_brand');
+    }
+}
